cdc462718e5c09d60a082d082308b0d89ffc63af
Simon Long <simon@raspberrypi.com>
2022-09-08 12:03:29 +0100

    Ignore all keyboard modifiers

--- plugins/menu/menu.c
+++ plugins/menu/menu.c
@@ -181,8 +181,8 @@ static gboolean handle_list_keypress (GtkWidget *widget, GdkEventKey *event, gpo
         return TRUE;
     }
 
-    if ((event->keyval >= 'a' && event->keyval <= 'z' && event->state == 0) ||
-        (event->keyval >= 'A' && event->keyval <= 'Z' && event->state == 1))
+    if ((event->keyval >= 'a' && event->keyval <= 'z') ||
+        (event->keyval >= 'A' && event->keyval <= 'Z'))
     {
         append_to_entry (m->srch, event->keyval);
         return TRUE;
@@ -372,8 +372,8 @@ static gboolean handle_key_presses (GtkWidget *widget, GdkEventKey *event, gpoin
         gtk_menu_popdown (GTK_MENU (m->menu));
         return TRUE;
     }
-    if ((event->keyval >= 'a' && event->keyval <= 'z' && event->state == 0) ||
-        (event->keyval >= 'A' && event->keyval <= 'Z' && event->state == 1))
+    if ((event->keyval >= 'a' && event->keyval <= 'z') ||
+        (event->keyval >= 'A' && event->keyval <= 'Z'))
     {
         if (!m->swin) do_search (m, event);
         else append_to_entry (m->srch, event->keyval);
