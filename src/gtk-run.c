8153194ec22925193d9acef2169c8eac359caf1a
Simon Long <simon@raspberrypi.com>
2025-05-27 09:43:59 +0100

    Remove GTK2 code

--- src/gtk-run.c
+++ src/gtk-run.c
@@ -342,11 +342,7 @@ static void on_entry_changed( GtkEntry* entry, GtkImage* img )
     }
     else
     {
-#if GTK_CHECK_VERSION(3, 0, 0)
         gtk_image_set_from_icon_name(img, "gtk-execute", GTK_ICON_SIZE_DIALOG);
-#else
-        gtk_image_set_from_stock(img, GTK_STOCK_EXECUTE, GTK_ICON_SIZE_DIALOG);
-#endif
     }
 }
 #endif
@@ -361,25 +357,6 @@ static void activate_window(GtkWindow* toplevel_window)
      * See EWMH spec, source indication part:
      * http://standards.freedesktop.org/wm-spec/wm-spec-latest.html#sourceindication
      */
-#if 0
-    GtkWidget* widget = GTK_WIDGET(toplevel_window);
-    GdkScreen* screen = gtk_widget_get_screen(widget);
-    if (gdk_x11_screen_supports_net_wm_hint (screen,
-					   gdk_atom_intern_static_string ("_NET_ACTIVE_WINDOW")))
-    {
-        GdkWindow* window = gtk_widget_get_window(widget);
-
-        /* show the window first */
-        gtk_widget_show(widget);
-
-        /* then, activate it */
-        Xclimsgx(GDK_SCREEN_XSCREEN(screen), GDK_WINDOW_XID(window),
-                 a_NET_ACTIVE_WINDOW, 2, /* source indication: 2 represents direct user actions. */
-                 gtk_get_current_event_time(),
-                 None /* currently active window */, 0, 0);
-    } /* if _NET_ACTIVE_WINDOW command is not supported */
-    else
-#endif
         gtk_window_present(toplevel_window);
 }
 
@@ -392,18 +369,9 @@ void gtk_run()
         win = gtk_dialog_new_with_buttons( _("Run"),
                                            NULL,
                                            0,
-#if GTK_CHECK_VERSION(3, 0, 0)
                                            _("_Cancel"), GTK_RESPONSE_CANCEL,
                                            _("_OK"), GTK_RESPONSE_OK,
-#else
-                                           GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                                           GTK_STOCK_OK, GTK_RESPONSE_OK,
-#endif
                                            NULL );
-#if !GTK_CHECK_VERSION(3, 0, 0)
-        gtk_dialog_set_alternative_button_order((GtkDialog*)win,
-                                GTK_RESPONSE_OK, GTK_RESPONSE_CANCEL, -1);
-#endif
         gtk_dialog_set_default_response( (GtkDialog*)win, GTK_RESPONSE_OK );
         entry = gtk_entry_new();
 
@@ -412,13 +380,8 @@ void gtk_run()
         gtk_box_pack_start( (GtkBox*)dlg_vbox,
                              gtk_label_new(_("Enter the command you want to execute:")),
                              FALSE, FALSE, 8 );
-#if GTK_CHECK_VERSION(3, 0, 0)
         hbox = gtk_box_new( GTK_ORIENTATION_HORIZONTAL, 2 );
         img = gtk_image_new_from_icon_name ( "gtk-execute", GTK_ICON_SIZE_DIALOG );
-#else
-        hbox = gtk_hbox_new( FALSE, 2 );
-        img = gtk_image_new_from_stock( GTK_STOCK_EXECUTE, GTK_ICON_SIZE_DIALOG );
-#endif
         gtk_box_pack_start( (GtkBox*)hbox, img,
                              FALSE, FALSE, 4 );
         gtk_box_pack_start( (GtkBox*)hbox, entry, TRUE, TRUE, 4 );
